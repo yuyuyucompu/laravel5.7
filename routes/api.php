8e50ebbb799a1fc739ef4bc808fb1cea1f60ae12
沈宇 <yu.shen@mail.daphne.com.cn>
2019-11-13 13:12:30 +0800

    Revert "jwt-api认证用户restful api"
    This reverts commit 13ad7a1f89b2eea4edc15b560b28518238e4103e.

--- routes/api.php
+++ routes/api.php
@@ -16,25 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::any('phpinfo', function () {
-   echo phpinfo();
-});
-
-Route::any('login', 'ApiController@login');
-Route::any('register', 'ApiController@register');
-
-// Route::any('register', function(){
-//     return 1;
-// });
-Route::group(['middleware' => 'auth.jwt'], function () {
-    Route::get('logout', 'ApiController@logout');
-
-    Route::get('user', 'ApiController@getAuthUser');
-
-    Route::get('products', 'ProductController@index');
-    Route::get('products/{id}', 'ProductController@show');
-    Route::post('products', 'ProductController@store');
-    Route::put('products/{id}', 'ProductController@update');
-    Route::delete('products/{id}', 'ProductController@destroy');
-});
